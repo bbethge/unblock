de89966c8292d72600ac8d2b343ac6cd436bc3b6
Ben Bethge <bethge931@gmail.com>
2017-04-11 09:52:04 -0500

    Added default values for rating fields

--- unblock/models.py
+++ unblock/models.py
@@ -1,13 +1,38 @@
 from django.db import models
 
 
+num_rows = 12
+num_columns = 6
+num_colors = 6  # Maximum 255
+max_stars = 5
+max_brains = 5
+
+
+class InvalidRatingError(Exception):
+    pass
+
+
 class Puzzle(models.Model):
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     # TODO: user that created it
-    stars = models.IntegerField()
-    ratings = models.IntegerField()
-    difficulty_points = models.IntegerField()
-    difficulty_ratings = models.IntegerField()
+    stars = models.IntegerField(default=0)
+    ratings = models.IntegerField(default=0)
+    difficulty_points = models.IntegerField(default=0)
+    difficulty_ratings = models.IntegerField(default=0)
     # TODO: actually store the tile pattern
     # TODO: figure out how to make the model depend on app configuration
+
+    def rate(self, num_stars):
+        if num_stars < 1 or num_stars > max_stars:
+            raise InvalidRatingError()
+        self.stars += num_stars
+        self.ratings += 1
+        self.save()
+
+    def rate_difficulty(self, num_brains):
+        if num_brains < 1 or num_brains > max_brains:
+            raise InvalidRatingError()
+        self.difficulty_points += num_brains
+        self.difficulty_ratings += 1
+        self.save()

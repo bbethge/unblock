5cf5bb95ba445075f5473783c3a9b8b17b113610
Ben Bethge <bethge931@gmail.com>
2017-04-11 13:10:28 -0500

    Created a simple detail view

--- unblock/urls.py
+++ unblock/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'unblock'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<puzzle_id>[0-9]+)/$', views.puzzle, name='puzzle'),
 ]
--- unblock/views.py
+++ unblock/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Puzzle
+from .models import Puzzle, num_rows, num_columns
 
 
 def index(request):
     latest_puzzle_list = Puzzle.objects.order_by('-pub_date')[:10]
     context = { 'latest_puzzle_list': latest_puzzle_list }
     return render(request, 'unblock/index.html', context)
+
+def puzzle(request, puzzle_id):
+    p = get_object_or_404(Puzzle, pk=puzzle_id)
+    context = {
+        'puzzle': p,
+        'tiles': [
+            [
+                int(p.tiles[num_columns*(num_rows-r-1)+c])
+                for c in range(num_columns)
+            ]
+            for r in range(num_rows)
+        ],
+    }
+    return render(request, 'unblock/puzzle.html', context)

1d41c7ff4b0ccd34d90d471765cff51cfb11395c
Ben Bethge <bethge931@gmail.com>
2017-04-14 19:58:08 -0500

    Made capitalization more consistent

--- unblock/static/unblock/play.js
+++ unblock/static/unblock/play.js
@@ -2,10 +2,10 @@ var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 var rows = tiles.length;
 var columns = tiles[0].length;
-var tile_width = canvas.width / columns;
-var tile_height = canvas.height / rows;
-var cursor_row = Math.round(rows / 2);
-var cursor_column = Math.round(columns / 2);
+var tileWidth = canvas.width / columns;
+var tileHeight = canvas.height / rows;
+var cursorRow = Math.round(rows / 2);
+var cursorColumn = Math.round(columns / 2);
 
 
 var colors =
@@ -19,26 +19,26 @@ function drawCanvas() {
             if (tiles[r][c] > 0) {
                 context.fillStyle = colors[tiles[r][c]-1];
                 context.fillRect(
-                    c * tile_width, r * tile_height, tile_width, tile_height
+                    c * tileWidth, r * tileHeight, tileWidth, tileHeight
                 );
             }
             else if (tiles[r][c] < 0) {
                 context.fillStyle = 'black';
                 context.fillRect(
-                    c * tile_width, r * tile_height, tile_width, tile_height
+                    c * tileWidth, r * tileHeight, tileWidth, tileHeight
                 );
             }
         }
     }
-    context.lineWidth = 2 * Math.round(tile_width / 25);
+    context.lineWidth = 2 * Math.round(tileWidth / 25);
     context.strokeStyle = 'white';
     context.strokeRect(
-        (cursor_column-1) * tile_width, cursor_row * tile_height,
-        tile_width, tile_height
+        (cursorColumn-1) * tileWidth, cursorRow * tileHeight,
+        tileWidth, tileHeight
     );
     context.strokeRect(
-        cursor_column * tile_width, cursor_row * tile_height,
-        tile_width, tile_height
+        cursorColumn * tileWidth, cursorRow * tileHeight,
+        tileWidth, tileHeight
     );
 }
 
@@ -75,18 +75,18 @@ canvas.onmousemove = function (event) {
     // TODO: check whether layerX/layerY are standard
     var x = event.layerX - canvas.offsetLeft;
     var y = event.layerY - canvas.offsetTop;
-    cursor_row = Math.min(Math.floor(y / tile_height), rows - 1);
-    cursor_column = Math.min(
-        Math.max(1, Math.round(x / tile_width)), columns - 1
+    cursorRow = Math.min(Math.floor(y / tileHeight), rows - 1);
+    cursorColumn = Math.min(
+        Math.max(1, Math.round(x / tileWidth)), columns - 1
     );
     drawCanvas();
 }
 
 canvas.onclick = function (event) {
     if (event.button == 0) {
-        var swap = tiles[cursor_row][cursor_column];
-        tiles[cursor_row][cursor_column] = tiles[cursor_row][cursor_column-1]
-        tiles[cursor_row][cursor_column-1] = swap;
+        var swap = tiles[cursorRow][cursorColumn];
+        tiles[cursorRow][cursorColumn] = tiles[cursorRow][cursorColumn-1]
+        tiles[cursorRow][cursorColumn-1] = swap;
         markDyingTiles();
         drawCanvas();
     }
